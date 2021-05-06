93e927a48cda9f13519f1e5a6cf2e30b516e4636
ayebravo <67244374+ayebravo@users.noreply.github.com>
2021-05-06 15:51:52 -0500

    Blah...
    The whole demo in one commit :(

--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { merge, race, EMPTY } from 'rxjs';
+import { expand, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SwapiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  loadPlanets() {
+    //return this.http.get("http://swapi.dev/api/planets/");
+
+    const pageOne = this.http.get("http://swapi.dev/api/planets/"); // Observable of an object
+
+    // Pipe to pass pageOne to other functions (expand and tap in this example); it returns another observable
+    return pageOne.pipe(
+      tap(x => console.log("tap", x)),
+      expand(
+        page => (page as any).next ? this.http.get((page as any).next) : EMPTY
+        // If there is a next page, make a request to it. If not, return {}
+      )
+    );
+
+
+    // const pageTwo = this.http.get("http://swapi.dev/api/planets/?page=2");
+
+    // return race(
+    //   pageOne,
+    //   pageTwo
+    // );
+
+    // return merge(
+    //   pageOne,
+    //   pageTwo
+    // );
+  };
 }
